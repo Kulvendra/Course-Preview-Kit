a0869f333f3d20064503e43c8b38600989aaa932
Kulvendra <kulvendra6749@gmail.com>
2020-05-01 16:53:35 -0400

    first commit

---
+++ src/temp.js
@@ -0,0 +1,368 @@
+var dir_tree = {
+    "type": "directory", 
+    "name": "", 
+    "children": [
+        {
+            "type": "directory", 
+            "name": "Demo Course", 
+            "children": [
+                {
+                    "type": "directory", 
+                    "name": "1.Hello world", 
+                    "children": []
+                }
+            ]
+        }, 
+        {
+            "type": "directory", 
+            "name": "React JS Web Development - The Essentials Bootcamp", 
+            "children": [
+                {
+                    "type": "directory", 
+                    "name": "3. Breaking down React  React and Web Development", 
+                    "children": [
+                        {
+                            "type": "file", 
+                            "name": "6. The DOM and React’s Virtual DOM.vtt"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "4. Refactor the Portfolio to use Parcel.vtt"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "2. React from Scratch.vtt"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "1. Section Overview.vtt"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "5. The Necessity of Bundlers and Transpilers Review.vtt"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "1. Section Overview.mp4"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "5. The Necessity of Bundlers and Transpilers Review.mp4"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "2. React from Scratch.mp4"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "3. Compiling, Bundling, and a Closer Look at JSX.vtt"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "3. Compiling, Bundling, and a Closer Look at JSX.mp4"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "6. The DOM and React’s Virtual DOM.mp4"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "4. Refactor the Portfolio to use Parcel.mp4"
+                        }
+                    ]
+                }, 
+                {
+                    "type": "directory", 
+                    "name": "2. Welcome to React  First React Application", 
+                    "children": [
+                        {
+                            "type": "file", 
+                            "name": "12. Component 2 Projects.mp4"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "2. Set up the Portfolio App.mp4"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "14. Challenge Social Profiles Component.vtt"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "10. A React Rule Never Directly Modify State.vtt"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "1. Section Overview  First React Application.mp4"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "4. ReactDOM, Elements, and JSX.mp4"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "6. Classes - Overview.mp4"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "9. SetState.mp4"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "3. Run the React App and the React Project Structure.vtt"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "4. ReactDOM, Elements, and JSX.vtt"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "5. A React Component.mp4"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "7. Classes, Inheritance, and a Closer Component Look.vtt"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "8. State.mp4"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "13. Props and Project Component.mp4"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "15. Code Social Profiles Component.vtt"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "7. Classes, Inheritance, and a Closer Component Look.mp4"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "16. Wrap up with Low-Hanging Styling Fruit.mp4"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "10. A React Rule Never Directly Modify State.mp4"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "9. SetState.vtt"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "13. Props and Project Component.vtt"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "3. Run the React App and the React Project Structure.mp4"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "1. Section Overview  First React Application.vtt"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "2. Set up the Portfolio App.vtt"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "5. A React Component.vtt"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "11. Class Properties and Initializers.mp4"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "16. Wrap up with Low-Hanging Styling Fruit.vtt"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "6. Classes - Overview.vtt"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "15. Code Social Profiles Component.mp4"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "11. Class Properties and Initializers.vtt"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "8. State.vtt"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "12. Component 2 Projects.vtt"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "14. Challenge Social Profiles Component.mp4"
+                        }
+                    ]
+                }, 
+                {
+                    "type": "file", 
+                    "name": "ReadMe.txt"
+                }, 
+                {
+                    "type": "directory", 
+                    "name": "1. Introduction", 
+                    "children": [
+                        {
+                            "type": "file", 
+                            "name": "2. A 7m Overview of Web Development and React.vtt"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "1. What You’ll Get From Taking this Course.vtt"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "2. A 7m Overview of Web Development and React.mp4"
+                        }, 
+                        {
+                            "type": "file", 
+                            "name": "1. What You’ll Get From Taking this Course.mp4"
+                        }
+                    ]
+                }, 
+                {
+                    "type": "file", 
+                    "name": "Course Downloaded from coursedrive.net.txt"
+                }, 
+                {
+                    "type": "file", 
+                    "name": "Visit Coursedrive.net.url"
+                }
+            ]
+        }
+    ]
+}
+getCourseName(dir_tree);
+
+var index=null;
+
+function getCourseName(main_dir) {
+
+
+    var total_course = dir_tree['children'].length;
+
+    for (i = 0; i < parseInt(total_course); i++) {
+
+        if (dir_tree['children'][i].type == "directory") {
+
+
+            var node = document.createElement("LI");
+            node.className = "list-group-item";
+            var textnode = document.createTextNode(dir_tree['children'][i].name);
+            node.appendChild(textnode);
+
+            var main = document.getElementById("courseList");
+            main.append(node);
+        }
+
+
+    }
+
+}
+
+var ul = document.getElementById('courseList');
+ul.onclick = function (event) {
+
+
+    var name = event.target.innerHTML;
+
+    var titleEle = document.getElementById("title");
+    titleEle.innerHTML = name;
+
+    var courseContainer = document.getElementById("courseContainer");
+    courseContainer.hidden = true;
+
+    var module = document.getElementById("module");
+    module.hidden = false;
+
+    var backButton = document.getElementById("backButton");
+    backButton.hidden = false;
+
+    getModuleList(name);
+
+};
+
+function backButton() {
+
+    var titleEle = document.getElementById("title");
+    titleEle.innerHTML = "My Courses";
+    var courseContainer = document.getElementById("courseContainer");
+    courseContainer.hidden = false;
+
+    var module = document.getElementById("module");
+    module.hidden = true;
+    var backButton = document.getElementById("backButton");
+    backButton.hidden = true;
+
+    var moduleList = document.getElementById("moduleList");
+
+    moduleList.innerHTML = '';
+
+    index=null;
+}
+
+function getModuleList(courseName) {
+
+    var moduleIndex = dir_tree['children'].findIndex(function (person) {
+
+        return person.name == courseName;
+    });
+
+    index = moduleIndex;
+
+    var moduleTree = dir_tree['children'][moduleIndex]['children'];
+
+
+    var moduleList = document.getElementById("moduleList");
+
+    var rawList = [];
+
+    for (i = 0; i < parseInt(moduleTree.length); i++) {
+        var name = moduleTree[i].name;
+
+        if (moduleTree[i].type == "directory") {
+            rawList.push(name);
+        }
+    }
+
+    rawList.sort();
+
+    for (i = 0; i < rawList.length; i++) {
+
+        var node = document.createElement("LI");
+        node.className = "list-group-item";
+        var textnode = document.createTextNode(rawList[i]);
+        node.appendChild(textnode);
+
+        moduleList.append(node);
+    }
+
+
+}
+
+
+var moduleEle = document.getElementById("moduleList");
+moduleEle.onclick = function (event) {
+
+
+    var name = event.target.innerHTML;
+
+    var currentLocation = window.location.toString().slice(0, -10);
+    var url = currentLocation + "home.html?name=" + encodeURIComponent(name+"#"+index);
+
+
+    window.location.href = url;
+
+};
+
